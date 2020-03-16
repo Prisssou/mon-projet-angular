30fc6d8d3fe587a7989a3b64a42b60bc8adf5296
Priscilla <priscilla.anciaux@gmail.com>
2020-03-16 17:11:47 +0100

    Components, Interpolation and Binding, Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mon-projet-angular';
+  isAuth = false;
+
+  appareils = [
+    {
+      name: 'Machine à laver',
+      status: 'éteint'
+    },
+    {
+      name: 'Frigo',
+      status: 'allumé'
+    },
+    {
+      name: 'Ordinateur',
+      status: 'éteint'
+    },
+
+  ]
+
+/*  appareilOne = 'Machine à laver';
+  appareilTwo = 'Frigo';
+  appareilThree = 'Ordinateur';
+
+  on = 'allumé';
+  off = 'éteint';*/
+
+  constructor() {
+    setTimeout(() => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+  onAllumer() {
+    console.log('On allume tout !');
+  }
+
 }
