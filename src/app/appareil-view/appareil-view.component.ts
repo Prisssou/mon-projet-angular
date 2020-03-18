35f20a32d14203dab6d76fbd67bd0022651df397
Priscilla <priscilla.anciaux@gmail.com>
2020-03-18 14:08:50 +0100

    server + end

--- src/app/appareil-view/appareil-view.component.ts
+++ src/app/appareil-view/appareil-view.component.ts
@@ -51,6 +51,14 @@ export class AppareilViewComponent implements OnInit, OnDestroy {
     }
   }
 
+  onSave() {
+    this.appareilService.saveAppareilToServer();
+  }
+
+  onFetch() {
+    this.appareilService.getAppareilsFromServer();
+  }
+
   ngOnDestroy() {
     this.appareilSubscription.unsubscribe();
   }
--- src/app/services/appareil.service.ts
+++ src/app/services/appareil.service.ts
@@ -1,17 +1,16 @@
-import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {Subject} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppareilService {
 
-  constructor() { }
-
   appareilSubject = new Subject<any[]>();
 
   private appareils = [
-    {
+/*    {
       id: 1,
       name: 'Machine à laver',
       status: 'éteint',
@@ -25,9 +24,12 @@ export class AppareilService {
       id: 3,
       name: 'Ordinateur',
       status: 'éteint',
-    },
+    },*/
 
   ];
+
+  constructor(private httpClient: HttpClient) {
+  }
 
   emitAppareilSubject() {
     this.appareilSubject.next(this.appareils.slice());
@@ -77,6 +79,38 @@ export class AppareilService {
     appareilObject.id = this.appareils[(this.appareils.length - 1)].id + 1;
     this.appareils.push(appareilObject);
     this.emitAppareilSubject();
+  }
+
+  saveAppareilToServer() {
+    this.httpClient
+    // La méthode post() renvoit un Observable
+    // .post('https://mon-projet-angular-a2519.firebaseio.com/appareils.json', this.appareils)
+
+    // La méthode put permet d'écraser l'enregistrement précédent
+      .put('https://mon-projet-angular-a2519.firebaseio.com/appareils.json', this.appareils)
+      .subscribe(
+        () => {
+          console.log('Enregistrement terminé !');
+        },
+        (error) => {
+          console.log('Erreur ! : ' + error);
+        }
+      );
+  }
+
+  getAppareilsFromServer() {
+    this.httpClient
+      .get<any[]>('https://mon-projet-angular-a2519.firebaseio.com/appareils.json')
+      .subscribe(
+        (response) => {
+          this.appareils = response;
+          this.emitAppareilSubject();
+        },
+        (error) => {
+          console.log('Erreur ! : ' + error);
+
+        }
+      );
   }
 
 }
